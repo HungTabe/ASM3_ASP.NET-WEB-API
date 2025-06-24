070b519bb80bfb58cd36379882484353da2f936c
tuantu04 <tuantu04@gmail.com>
2025-06-24 17:34:29 +0700

    [asm2_5] Develop Service Layer

---
+++ TranDinhHung_PRN232_A02_BE/FUNewsManagementSystem.Business/IServices/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using FUNewsManagementSystem.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsManagementSystem.Business.IServices
+{
+    public interface IAccountService
+    {
+        Task<SystemAccount> AuthenticateAsync(string email, string password);
+        Task<IEnumerable<SystemAccount>> GetAllAsync();
+        Task<SystemAccount> GetByIdAsync(short id);
+        Task CreateAsync(SystemAccount account);
+        Task UpdateAsync(SystemAccount account);
+        Task<bool> DeleteAsync(short id);
+    }
+}
---
+++ TranDinhHung_PRN232_A02_BE/FUNewsManagementSystem.Business/Services/NewsArticleService.cs
@@ -0,0 +1,58 @@
+﻿using FUNewsManagementSystem.Business.IServices;
+using FUNewsManagementSystem.Data.Entities;
+using FUNewsManagementSystem.Data.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsManagementSystem.Business.Services
+{
+    public class NewsArticleService : INewsArticleService
+    {
+        private readonly INewsArticleRepository _newsArticleRepository;
+        private readonly ITagRepository _tagRepository;
+
+        public NewsArticleService(INewsArticleRepository newsArticleRepository, ITagRepository tagRepository)
+        {
+            _newsArticleRepository = newsArticleRepository;
+            _tagRepository = tagRepository;
+        }
+
+        public async Task<List<NewsArticle>> GetAllAsync()
+        {
+            return await _newsArticleRepository.GetAllAsync();
+        }
+
+        public async Task<NewsArticle> GetByIdAsync(string id)
+        {
+            return await _newsArticleRepository.GetByIdAsync(id);
+        }
+
+        public async Task CreateAsync(NewsArticle article, List<int> tagIds)
+        {
+            await _newsArticleRepository.CreateAsync(article);
+            if (tagIds != null && tagIds.Any())
+            {
+                await _newsArticleRepository.AddTagsAsync(article.NewsArticleId, tagIds);
+            }
+        }
+
+        public async Task UpdateAsync(NewsArticle article, List<int> tagIds)
+        {
+            await _newsArticleRepository.UpdateAsync(article);
+            await _newsArticleRepository.RemoveTagsAsync(article.NewsArticleId);
+            if (tagIds != null && tagIds.Any())
+            {
+                await _newsArticleRepository.AddTagsAsync(article.NewsArticleId, tagIds);
+            }
+        }
+
+        public async Task<bool> DeleteAsync(string id)
+        {
+            await _newsArticleRepository.RemoveTagsAsync(id); // Xóa Tags trước
+            return await _newsArticleRepository.DeleteAsync(id);
+        }
+    }
+}
