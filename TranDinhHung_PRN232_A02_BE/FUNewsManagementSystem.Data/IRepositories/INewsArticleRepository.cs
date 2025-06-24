6473338c4445a768309e01749e7bcf24253af45d
tuantu04 <tuantu04@gmail.com>
2025-06-24 17:10:56 +0700

    [asm2_3] Setup IRepository and Repo but not have NewsTag

---
+++ TranDinhHung_PRN232_A02_BE/FUNewsManagementSystem.Data/IRepositories/INewsArticleRepository.cs
@@ -0,0 +1,18 @@
+﻿using FUNewsManagementSystem.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsManagementSystem.Data.IRepositories
+{
+    public interface INewsArticleRepository
+    {
+        Task<List<NewsArticle>> GetAllAsync();
+        Task<NewsArticle> GetByIdAsync(string id);
+        Task CreateAsync(NewsArticle article);
+        Task UpdateAsync(NewsArticle article);
+        Task<bool> DeleteAsync(string id);
+    }
+}
---
+++ TranDinhHung_PRN232_A02_BE/FUNewsManagementSystem.Data/Repositories/SystemAccountRepository.cs
@@ -0,0 +1,50 @@
+﻿using FUNewsManagementSystem.Data.Basic;
+using FUNewsManagementSystem.Data.Data;
+using FUNewsManagementSystem.Data.Entities;
+using FUNewsManagementSystem.Data.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsManagementSystem.Data.Repositories
+{
+    public class SystemAccountRepository : GenericRepository<SystemAccount>, ISystemAccountRepository
+    {
+        public SystemAccountRepository(FunewsManagementA2Context context) : base(context) { }
+
+        public async Task<List<SystemAccount>> GetAllAsync()
+        {
+            return await _context.SystemAccounts.ToListAsync();
+        }
+
+        public async Task<SystemAccount> GetByIdAsync(short id)
+        {
+            return await _context.SystemAccounts.FindAsync(id);
+        }
+
+        public async Task CreateAsync(SystemAccount account)
+        {
+            await CreateAsync(account);
+        }
+
+        public async Task UpdateAsync(SystemAccount account)
+        {
+            await UpdateAsync(account);
+        }
+
+        public async Task<bool> DeleteAsync(short id)
+        {
+            var account = await GetByIdAsync(id);
+            if (account == null || !await CanDeleteAsync(id)) return false;
+            return await RemoveAsync(account);
+        }
+
+        public async Task<bool> CanDeleteAsync(short id)
+        {
+            return !await _context.NewsArticles.AnyAsync(n => n.CreatedById == id || n.UpdatedById == id);
+        }
+    }
+}
